89b72853dfe524ce8321926a308b65d9eff95287
rudi97277 <rudi97277@gmail.com>
2024-07-15 21:15:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        dark: "#171923",
+        complement: "#f3392e",
+      },
+    },
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#171923",
+        complement: "#f3392e",
+      },
+    },
+  },
+};
